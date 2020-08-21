68d4653f0830b824a67fdf317123d622da32f6b9
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-08-21 20:44:23 -0300

    Create Tomadas.cpp

---
+++ Tomadas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+ 
+using namespace std;
+ 
+int main(int argc, char** argv)
+{
+	int soma = 0;
+	int t1, t2, t3, t4;
+	
+	scanf("%d %d %d %d",&t1, &t2, &t3, &t4);
+	printf("%d\n", ((t1+t2+t3 + t4) - 3));
+	
+	return 0;
+}
