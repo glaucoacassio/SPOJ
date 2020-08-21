1b88829d87ad2762774134d45f5fb1099c5cd1a8
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-08-21 20:52:10 -0300

    Create Bafo.c

---
+++ Bafo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+int main(void) {
+	int i, cont = 0, somaA, somaB;
+	int numero, aldo, beto;
+	
+//	while(scanf("%d", &numero) && numero != 0) {
+	while(1) {
+		scanf("%d", &numero);
+		
+		if(numero == 0)
+			break;
+		 
+		somaA = 0;
+		somaB = 0;
+		
+		for(i = 0; i<numero; i++) {
+			scanf("%d %d", &aldo, &beto);
+			somaA += aldo;
+			somaB += beto;
+		}
+		
+		somaA > somaB ? printf("\nTeste %d\nAldo\n\n", ++cont) : printf("\nTeste %d\nBeto\n\n", ++cont); 
+	}
+ 
+	return EXIT_SUCCESS;
+}//fim do programa
+ 
