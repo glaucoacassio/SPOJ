8daa3e095cbf02c61fcd6a955daad4c668c4ed79
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-08-21 20:54:03 -0300

    Create Quadrados.c

---
+++ Quadrados.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAM 5
+ 
+int main(void) {
+	int numero;
+	
+	scanf("%d",  &numero);
+	printf("%d\n", numero*numero);
+	
+	return EXIT_SUCCESS;
+}
